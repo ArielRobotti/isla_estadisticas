03a09d627e21db42e7e49ea889f8ed7e146d1ff9
ariel robotti <arielrobotti@gmail.com>
2026-04-21 08:50:59 -0300

    styles

--- src/frontend_react/src/hooks/useIslandAnalytics.ts
+++ src/frontend_react/src/hooks/useIslandAnalytics.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
-// import { type IslandData } from '../types/metrics';
 
 export const useIslandAnalytics = () => {
   const [data, setData] = useState<any>(null); // Métricas
